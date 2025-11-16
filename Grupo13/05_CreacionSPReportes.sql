73e5c3701c0975329887168506eabeb5ef259f18
FeduuML <fedeliber@live.com>
2025-11-16 13:07:26 -0300

    Reporte 5 sin terminar
    Faltan definir mas parámetros de filtro

---
+++ Grupo13/05_CreacionSPReportes.sql
@@ -0,0 +1,55 @@
+/* =========================================================
+	05_CreacionSPReportes.sql - Com2900G13
+	Proyecto: Altos de Saint Just
+	Materia: Bases de datos aplicada
+    Grupo: 13
+
+	Este archivo crea los SP de los reportes
+
+	Alumnos:
+		45628269 - Liber Federico Manuel 
+		46265307 - Ares Nicolás jesús 
+		45754471 - Pityla Damian 
+		42587858 - Murillo Joel
+		46292592 - Larriba Pedro Ezequiel 
+		40464246 - Diaz Ortiz  Lucas Javier 
+========================================================= */
+
+USE Com2900G13
+GO
+
+------------------------------ Reporte 1 -----------------------------
+
+------------------------------ Reporte 2 -----------------------------
+
+------------------------------ Reporte 3 -----------------------------
+
+------------------------------ Reporte 4 -----------------------------
+
+------------------------------ Reporte 5 -----------------------------
+
+CREATE OR ALTER PROCEDURE bda.spMostrarTOP3Morosos
+	@NombreConsorcio VARCHAR(80)
+AS
+	SET NOCOUNT ON;
+
+	WITH Propietarios_Inquilinos_UF AS(
+		SELECT * FROM bda.Propietario_en_UF
+		UNION
+		SELECT * FROM bda.Inquilino_en_UF
+	),
+	Propietarios_Inquilinos AS(
+		SELECT * FROM bda.Propietario
+		UNION
+		SELECT * FROM bda.Inquilino
+	)
+	SELECT TOP 3 p_i.Nombre,p_i.Apellido,p_i.DNI,p_i.Email,p_i.Telefono,piuf.CVU_CBU_Propietario,de.deuda,c.nombre,uf.numero_unidad FROM bda.Detalle_Expensa de
+	INNER JOIN Propietarios_Inquilinos_UF piuf ON de.id_uf = piuf.id_uf
+	INNER JOIN Propietarios_Inquilinos p_i ON piuf.CVU_CBU_Propietario = p_i.CVU_CBU
+	INNER JOIN bda.Unidad_Funcional uf ON piuf.id_uf = uf.id_unidad
+	INNER JOIN bda.Consorcio c ON uf.id_consorcio = c.id_consorcio
+	WHERE (c.nombre = @NombreConsorcio OR @NombreConsorcio IS NULL) --Esta linea permite el filtrado opcional
+	ORDER BY deuda DESC
+GO
+
+------------------------------ Reporte 6 -----------------------------
---
+++ Grupo13/06_EjecucionSPReportes.sql
@@ -0,0 +1,34 @@
+/* =========================================================
+	06_EjecucionSPReportes.sql - Com2900G13
+	Proyecto: Altos de Saint Just
+	Materia: Bases de datos aplicada
+    Grupo: 13
+
+	Este archivo ejecuta los SP de los reportes
+
+	Alumnos:
+		45628269 - Liber Federico Manuel 
+		46265307 - Ares Nicolás jesús 
+		45754471 - Pityla Damian 
+		42587858 - Murillo Joel
+		46292592 - Larriba Pedro Ezequiel 
+		40464246 - Diaz Ortiz  Lucas Javier 
+========================================================= */
+
+USE Com2900G13
+GO
+
+------------------------------ Reporte 1 -----------------------------
+
+------------------------------ Reporte 2 -----------------------------
+
+------------------------------ Reporte 3 -----------------------------
+
+------------------------------ Reporte 4 -----------------------------
+
+------------------------------ Reporte 5 -----------------------------
+
+EXEC bda.spMostrarTOP3Morosos
+	@NombreConsorcio = 'Azcuenaga'
+
+------------------------------ Reporte 6 -----------------------------
